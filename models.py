8cadd423d9a9833e30157c2ed669f62d7e80e810
Andrey Bezpalov <abezpalov@ya.ru>
2015-05-14 20:04:46 +0400

    Добавлена сущность Логи

--- models.py
+++ models.py
@@ -12,7 +12,9 @@ class Category(models.Model):
 	path        = models.CharField(max_length=512, null=True)
 	state       = models.BooleanField(default=True)
 	created     = models.DateTimeField()
+	created_by  = models.CharField(max_length=100, null=True, default=None)
 	modified    = models.DateTimeField()
+	modified_by = models.CharField(max_length=100, null=True, default=None)
 
 	def __str__(self):
 		return self.name
@@ -25,8 +27,10 @@ class DocType(models.Model):
 	name     = models.CharField(max_length=100)
 	alias    = models.CharField(max_length=100)
 	state    = models.BooleanField(default=True)
-	created  = models.DateTimeField()
-	modified = models.DateTimeField()
+	created      = models.DateTimeField()
+	created_by   = models.CharField(max_length=100, null=True, default=None)
+	modified     = models.DateTimeField()
+	modified_by  = models.CharField(max_length=100, null=True, default=None)
 
 	def __str__(self):
 		return self.alias
@@ -39,8 +43,10 @@ class Language(models.Model):
 	name     = models.CharField(max_length=100)
 	alias    = models.CharField(max_length=100)
 	state    = models.BooleanField(default=True)
-	created  = models.DateTimeField()
-	modified = models.DateTimeField()
+	created      = models.DateTimeField()
+	created_by   = models.CharField(max_length=100, null=True, default=None)
+	modified     = models.DateTimeField()
+	modified_by  = models.CharField(max_length=100, null=True, default=None)
 
 	def __str__(self):
 		return self.alias
@@ -48,37 +54,59 @@ class Language(models.Model):
 	class Meta:
 		ordering = ['alias']
 
-# Content
-class Content(models.Model):
-	title        = models.CharField(max_length=100)
-	alias        = models.CharField(max_length=100, null=True)
-	patch        = models.CharField(max_length=512, null=True)
-	src          = models.CharField(max_length=512, null=True)
-	thumb_src    = models.CharField(max_length=512, null=True)
-	doc_type     = models.ForeignKey(DocType, null=True, default=None)
+# Article
+class Article(models.Model):
+	title        = models.CharField(max_length=100)                             # Заголовок
+	alias        = models.CharField(max_length=100, null=True)                  # Псевдоним (служебное поле)
+	patch        = models.CharField(max_length=512, null=True)                  # Путь
+	thumb_src    = models.CharField(max_length=512, null=True)                  # Ссылка на изображение-предпросмотр
 
-	intro        = models.TextField()
-	content      = models.TextField()
-	description  = models.TextField()
-	banner_main  = models.TextField()
-	banner_side  = models.TextField()
+	intro        = models.TextField()                                           # Вводный текст
+	content      = models.TextField()                                           # Текст статьи
+	description  = models.TextField()                                           # Описание
 
-	category     = models.ForeignKey(Category, null=True, default=None)
-	language     = models.ForeignKey(Language, null=True, default=None)
+	category     = models.ForeignKey(Category, null=True, default=None)         # Категория
+	language     = models.ForeignKey(Language, null=True, default=None)         # Язык
 
-	source       = models.CharField(max_length=512, null=True)
-	source_url   = models.CharField(max_length=512, null=True)
+	source       = models.CharField(max_length=512, null=True)                  # Источник
+	source_url   = models.CharField(max_length=512, null=True)                  # Ссылка на источник
 
-	state        = models.BooleanField(default=True)
-	on_main      = models.BooleanField(default=False)
-	on_side      = models.BooleanField(default=False)
-	on_news      = models.BooleanField(default=False)
-	power        = models.IntegerField(default=1)
+	state        = models.BooleanField(default=True)                            # Статус (опубликована ли статья)
+	on_main      = models.BooleanField(default=False)                           # На главной ли статья
 
 	created      = models.DateTimeField()
 	created_by   = models.CharField(max_length=100, null=True, default=None)
 	modified     = models.DateTimeField()
 	modified_by  = models.CharField(max_length=100, null=True, default=None)
+	published    = models.DateTimeField(null=True, default=None)
+	published_by = models.CharField(max_length=100, null=True, default=None)
+	pub_from     = models.DateTimeField(null=True, default=None)
+	pub_to       = models.DateTimeField(null=True, default=None)
+
+	def __str__(self):
+		return self.title
+
+# Document
+class Document(models.Model):
+	title        = models.CharField(max_length=100)                             # Заголовок
+	alias        = models.CharField(max_length=100, null=True)                  # Псевдоним (служебное поле)
+	patch        = models.CharField(max_length=512, null=True)                  # Путь
+	src          = models.CharField(max_length=512, null=True)                  # Ссылка на расположение
+	thumb_src    = models.CharField(max_length=512, null=True)                  # Ссылка на изображение-предпросмотр
+	doc_type     = models.ForeignKey(DocType, null=True, default=None)          # Тип документа
+
+	description  = models.TextField()                                           # Описание
+
+	category     = models.ForeignKey(Category, null=True, default=None)         # Категория
+	language     = models.ForeignKey(Language, null=True, default=None)         # Язык
+
+	source       = models.CharField(max_length=512, null=True)                  # Источник
+	source_url   = models.CharField(max_length=512, null=True)                  # Ссылка на источник
+
+	state        = models.BooleanField(default=True)
+
+	created      = models.DateTimeField()
+	created_by   = models.CharField(max_length=100, null=True, default=None)
 	published    = models.DateTimeField(null=True, default=None)
 	published_by = models.CharField(max_length=100, null=True, default=None)
 	pub_from     = models.DateTimeField()
@@ -87,21 +115,48 @@ class Content(models.Model):
 	def __str__(self):
 		return self.title
 
-# Menu
-class Menu(models.Model):
-	name        = models.CharField(max_length=100)
-	alias       = models.CharField(max_length=100)
-	description = models.TextField()
-	parent      = models.ForeignKey('self', null=True, default=None)
-	level       = models.IntegerField()
-	order       = models.IntegerField()
-	path        = models.CharField(max_length=512, null=True)
-	state       = models.BooleanField(default=True)
-	created     = models.DateTimeField()
-	modified    = models.DateTimeField()
+# Document Collection
+class DocumentCollection(models.Model):
+	title        = models.CharField(max_length=100)
+	alias        = models.CharField(max_length=100, null=True)
+	state        = models.BooleanField(default=True)
+	created      = models.DateTimeField()
+	created_by   = models.CharField(max_length=100, null=True, default=None)
+	modified     = models.DateTimeField()
+	modified_by  = models.CharField(max_length=100, null=True, default=None)
+
+	to_article   = models.ManyToManyField(Article,  db_table='project_collection_to_article')
+	to_document  = models.ManyToManyField(Document, db_table='project_collection_to_document')
 
 	def __str__(self):
-		return self.name
+		return self.title
+
+# Log manager
+class LogManager(models.Manager):
+
+	# TODO add
+	def add(self, subject, channel, title, description):
+
+		log = Log(
+			subject     = subject[:100],
+			channel     = channel[:100],
+			title       = title[:100],
+			description = description,
+			created     = timezone.now())
+		log.save()
+		return log
+
+# Log
+class Log(models.Model):
+	subject     = models.CharField(max_length=100)
+	channel     = models.CharField(max_length=100)
+	title       = models.CharField(max_length=100)
+	description = models.TextField()
+	created     = models.DateTimeField()
+	objects     = LogManager()
+
+	def __str__(self):
+		return "{} | {} | {}".format(self.subject, self.channel, self.title) 
 
 	class Meta:
-		ordering = ['order']
+		ordering = ['-created']
